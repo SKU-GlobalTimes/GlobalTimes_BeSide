b7d4083a91642c4ab0d02c5665e1250b0eda19b8
Oyisbe <onyu8407@naver.com>
2025-03-31 01:35:23 +0900

    :sparkles: Feat: Article 최신순, 조회수 순 조회 API 추가
    - API 응답 관련 DTO 추가
    - 서비스 로직 및 관련 Repository 조회 메소드 추가
    - SwaggerDocs 적용

---
+++ src/main/java/com/example/globalTimes_be/controller/ArticleController.java
@@ -0,0 +1,46 @@
+package com.example.globalTimes_be.controller;
+
+import com.example.globalTimes_be.ApiDoc.ArticleApiDocumentation;
+import com.example.globalTimes_be.Service.ArticleService;
+import com.example.globalTimes_be.dto.ArticleResponseDto;
+import com.example.globalTimes_be.global.apiPayload.code.ApiResponse;
+import com.example.globalTimes_be.global.apiPayload.code.status.GlobalSuccessStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/articles")
+@RequiredArgsConstructor
+public class ArticleController implements ArticleApiDocumentation {
+
+    private final ArticleService articleService;
+
+    // 최신순
+    @Override
+    @GetMapping("/latest")
+    public ResponseEntity<?> getArticlesByPublishedAt(
+            @RequestParam(name = "page") int page,
+            @RequestParam(name = "size") int size) {
+
+        Page<ArticleResponseDto> articles = articleService.getArticlesByPublishedAt(page, size);
+
+        return ApiResponse.success(GlobalSuccessStatus._OK.getResponse(), articles);
+    }
+
+    // 조회수 높은 순
+    @Override
+    @GetMapping("/popular")
+    public ResponseEntity<?> getArticlesByViewCount(
+            @RequestParam(name = "page") int page, // page와 size는 필수로 클라이언트가 제공
+            @RequestParam(name = "size") int size) {
+
+        Page<ArticleResponseDto> articles = articleService.getArticlesByViewCount(page, size);
+
+        return ApiResponse.success(GlobalSuccessStatus._OK.getResponse(), articles);
+    }
+}
