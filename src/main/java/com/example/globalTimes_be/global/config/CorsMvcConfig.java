983e00b850a4d7e8ab946e6e5092d80b9bacca96
Oyisbe <onyu8407@naver.com>
2025-04-08 16:21:53 +0900

    :rocket: Refactor: CorsMvcConfig 수정
    - * 와일드 카드 전면 허용을 특정 Origin (배포환경)만 설정하도록 함.

--- src/main/java/com/example/globalTimes_be/global/config/CorsMvcConfig.java
+++ src/main/java/com/example/globalTimes_be/global/config/CorsMvcConfig.java
@@ -9,8 +9,12 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
-                .allowedOriginPatterns("*") // 모든 Origin 허용 (특정 Origin이면 allowedOrigins 사용)
-                .allowCredentials(true); // 쿠키, 인증 정보 포함 가능
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins(
+                        "https://global-times.netlify.app",
+                        "https://globaltimes.store",
+                        "https://www.globaltimes.store"
+                )
+                .allowCredentials(true);
     }
 }
